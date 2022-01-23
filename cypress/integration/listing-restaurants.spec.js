fcf690ae68f666112fab6d0c3da649dc9a801856
Reese Kim <reesekimm@gmail.com>
2022-01-23 20:03:02 +0900

    Blue(E2E): Cannot read properties of undefined (reading 'then')

--- cypress/integration/listing-restaurants.spec.js
+++ cypress/integration/listing-restaurants.spec.js
@@ -3,7 +3,7 @@ describe('Listing Restaurants', () => {
     const sushiPlace = 'Sushi Place'
     const pizzaPlace = 'Pizza Place'
 
-    cy.intercept('GET', 'https://outside-in-dev-api.herokuapp.com/vg8dRmI6Exi8e57MGUmwCjWWWrVzuafm/retaurants', {
+    cy.intercept('GET', 'https://outside-in-dev-api.herokuapp.com/KP1YaFHIrRhdeL6c5o5wngkYYqwZPt8s/restaurants', {
       body: [
         { id: 1, name: sushiPlace },
         { id: 2, name: pizzaPlace },
