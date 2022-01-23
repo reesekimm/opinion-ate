e63eab98eb31f5a88c3d0cf975b852737b2b0e4e
Reese Kim <reesekimm@gmail.com>
2022-01-23 16:53:54 +0900

    Green(unit) : Load restaurants upon first rendering RestaurantList

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,4 +1,10 @@
-const RestaurantList = () => {
+import { useEffect } from 'react'
+
+export const RestaurantList = ({ loadRestaurants }) => {
+  useEffect(() => {
+    loadRestaurants()
+  }, [loadRestaurants])
+
   return <div>RestaurantList</div>
 }
 
