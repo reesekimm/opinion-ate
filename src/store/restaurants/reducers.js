4a38fac699d3d36a2df077c81b87b1f227e7d46d
Reese Kim <reesekimm@gmail.com>
2022-01-23 19:37:00 +0900

    Green(unit): Add loadRestaurants action

--- src/store/restaurants/reducers.js
+++ src/store/restaurants/reducers.js
@@ -1,5 +1,13 @@
 import { combineReducers } from 'redux'
+import { STORE_RESTAURANTS } from './actions'
 
-const records = () => []
+const records = (state = [], action) => {
+  switch (action.type) {
+    case STORE_RESTAURANTS:
+      return action.records
+    default:
+      return state
+  }
+}
 
 export default combineReducers({ records })
